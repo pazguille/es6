97e333239799514024c60aa5537e38e5a639c3af
Guille Paz <guille87paz@gmail.com>
2014-12-21 15:19:27 -0300

    Create extend module.

--- src/extend.js
+++ src/extend.js
@@ -1,5 +1,22 @@
 'use strict';
 
+/**
+ * Extends a given object with properties from another object.
+ * @param {Object} to - A given object to extend its properties.
+ * @param {Object} from - A given object to share its properties.
+ * @returns {Object} to
+ * @example
+ * var a = {
+ *   'foo': 'bar'
+ * };
+ * var b = {
+ *   'bar': 'foo'
+ * };
+ *
+ * extend(a, b);
+ *
+ * console.log(a.bar) // returns 'foo'
+ */
 function extend(to, from) {
   for (let prop in from) {
     if (from[prop]) {
@@ -9,4 +26,5 @@ function extend(to, from) {
   return to;
 }
 
+// Exports extend
 export default extend;
