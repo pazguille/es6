eef3ddeff279966db85f2bee3fb893abaf5600c3
Guille Paz <guille87paz@gmail.com>
2014-12-22 12:39:45 -0300

    Add jsdoc comments.

--- src/inherit.js
+++ src/inherit.js
@@ -2,6 +2,14 @@
 
 import extend from './src/extend';
 
+/**
+ * Inherits prototype properties from `uber` into `child` constructor.
+ * @param {Function} child - A given constructor function who inherits.
+ * @param {Function} uber - A given constructor function to inherit.
+ * @returns {Object}
+ * @example
+ * inherit(child, uber);
+ */
 function inherit(child, uber) {
   let obj = child.prototype || {};
   child.prototype = extend(obj, uber.prototype);
