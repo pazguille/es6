a942679f9ee622655e59e1027b7da926cb08fc85
Guille Paz <guille87paz@gmail.com>
2014-12-22 12:19:36 -0300

    Update builder.js

--- builder.js
+++ builder.js
@@ -7,9 +7,9 @@ var to5ify = require('6to5ify');
 
 mkdirp.sync('./dist');
 
-browserify({debug: true})
+browserify({'debug': true, 'standalone': 'es6'})
   .transform(to5ify)
-  .require('./index.js', {entry: true})
+  .require('./index.js', {'entry': true})
   .bundle()
   .on('error', function (err) { console.log('Error : ' + err.message); })
   .pipe(fs.createWriteStream('dist/build.js'));
